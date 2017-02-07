211cc250d4064c53eba452488555a38e58cfdeba
thesharpshooter <prakash9266@gmail.com>
2017-02-07 23:14:58 +0530

    .

---
+++ 03 - CSS Variables/scripts.js
@@ -0,0 +1,7 @@
+const inputs = document.querySelectorAll('input');
+var image = document.querySelector('img');
+function handler(){
+	document.element
+}
+inputs.forEach(input => input.addEventListener('change',handler));
+inputs.forEach(input => input.addEventListener('mousemove',handler));
